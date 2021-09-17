74fa18c6a8a6ed0aa5780848514217b592c92cb6
Binyan <lbyxiafei@gmail.com>
2021-09-17 16:10:39 -0700

    Refactor & add template.

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+#ifdef _LBY
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+#endif
+
+    string s;
+    cin >> s;
+    cout << s;
+
+
+    return 0;
+}
