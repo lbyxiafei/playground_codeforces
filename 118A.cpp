e0eeea85ac27fa5ade066c0e95967b138aae6a14
Binyan <lbyxiafei@gmail.com>
2021-09-17 14:44:03 -0700

    Add git ignore.

--- 118A.cpp
+++ 118A.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 int main(){
-    freopen("input.txt","r",stdin);
+    //freopen("input.txt","r",stdin);
+    cout << _DEBUG;
 
     string s,res;
     cin >> s;
-    unordered_set<char> V{'a','e','i','o','u','A','E','I','O','U'};
+    unordered_set<char> V{'a','e','i','o','u','A','E','I','O','U','y','Y'};
 
     for(char ch:s){
         if(V.count(ch)) continue;
         res+='.';
         res+=tolower(ch);
     }
-    cout << res;
+    cout << res << endl;
 
     return 0;
 }
