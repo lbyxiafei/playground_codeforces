01a0754437e75cff612283cad5365c9e517e559b
Binyan <lbyxiafei@gmail.com>
2021-11-02 19:10:39 -0700

    Formatting.

--- CP/CF.cpp
+++ CP/CF.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
@@ -10,31 +11,35 @@ typedef tree<
     null_type,
     less<int>,
     rb_tree_tag,
-    tree_order_statistics_node_update> ordered_set;
+    tree_order_statistics_node_update>
+    ordered_set;
 typedef tree<
-    pair<int,int>,
+    pair<int, int>,
     null_type,
-    less<pair<int,int>>,
+    less<pair<int, int>>,
     rb_tree_tag,
-    tree_order_statistics_node_update> ordered_multiset;
+    tree_order_statistics_node_update>
+    ordered_multiset;
 typedef long long LL;
 typedef unsigned long long ULL;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 
-int main(){
+int main() {
 #ifdef _LBY
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
 #endif
 
-    ordered_set S;
-    vector<int>nums{1,3,2,2,4,4,8,6,7,3};
-    for(int i=0; i<nums.size(); i++){
-        S.insert(nums[i]);
-        cout << nums[i] << ':' << S.order_of_key(nums[i]) << endl;
-    }
-    for(auto x:S) cout << x << ' ';
-    cout << endl << *S.find_by_order(0) << ',' << *S.lower_bound(5) << endl;
+  ordered_set S;
+  vector<int> nums{1, 3, 2, 2, 4, 4, 8, 6, 7, 3};
+  for (int i = 0; i < nums.size(); i++) {
+    S.insert(nums[i]);
+    cout << nums[i] << ':' << S.order_of_key(nums[i]) << endl;
+  }
+  for (auto x : S)
+    cout << x << ' ';
+  cout << endl
+       << *S.find_by_order(0) << ',' << *S.lower_bound(5) << endl;
 
-    return 0;
+  return 0;
 }
--- CP/T.cpp
+++ CP/T.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
@@ -10,49 +11,49 @@ typedef tree<
     null_type,
     less<int>,
     rb_tree_tag,
-    tree_order_statistics_node_update> ordered_set;
+    tree_order_statistics_node_update>
+    ordered_set;
 typedef tree<
-    pair<int,int>,
+    pair<int, int>,
     null_type,
-    less<pair<int,int>>,
+    less<pair<int, int>>,
     rb_tree_tag,
-    tree_order_statistics_node_update> ordered_multiset;
+    tree_order_statistics_node_update>
+    ordered_multiset;
 typedef long long LL;
 typedef unsigned long long ULL;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 
-template<typename T>
-T add(T a, T b){
-    return a+b;
+template <typename T>
+T add(T a, T b) {
+  return a + b;
 }
 
-class LiVec{
-
+class LiVec {
 };
 
-bool check(int x){
-    string s=to_string(x);
-    vector<int> f(10);
-    for(char ch:s)
-        f[ch-'0']++;
-    for(int i=0; i<10; i++)
-        if(f[i] && f[i]!=i)
-            return false;
-    return true;
+bool check(int x) {
+  string s = to_string(x);
+  vector<int> f(10);
+  for (char ch : s)
+    f[ch - '0']++;
+  for (int i = 0; i < 10; i++)
+    if (f[i] && f[i] != i)
+      return false;
+  return true;
 }
 
-int main(){
+int main() {
 #ifdef _LBY
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
 #endif
 
-    set<int> S{10,20};
-    auto it=S.lower_bound(15);
-    cout << '!';
-    cout << distance(S.begin(),it);
-    cout << '!';
-    
+  set<int> S{10, 20};
+  auto it = S.lower_bound(15);
+  cout << '!';
+  cout << distance(S.begin(), it);
+  cout << '!';
 
-    return 0;
+  return 0;
 }
