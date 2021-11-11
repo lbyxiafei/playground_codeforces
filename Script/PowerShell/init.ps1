b7dc808de86dd8f784dfdec91b50828ba2f16e1a
Binyan Li <binyanli@microsoft.com>
2021-11-11 15:15:54 -0800

    Check point, 2021-11-11T15:15:54.

--- Script/PowerShell/init.ps1
+++ Script/PowerShell/init.ps1
@@ -1,3 +1,10 @@
+$prepo=$env:userprofile+"\Repos\Templates"
+$pstage=$env:userprofile+"\Repos\Templates\Script\PowerShell"
+$plocal=$env:userprofile+"\Scripts"
+$ts=Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
+$pdump_local=$env:userprofile+"\Downloads\Dump\Script\Local\"+$ts
+$pdump_repo=$env:userprofile+"\Downloads\Dump\Script\Repo\"+$ts
+
 # System::Path: $Get-EnvPath Machine
 # User::Path: $Get-EnvPath User
 function Get-EnvPath {
@@ -79,24 +86,30 @@ function Add-EnvPath {
     }
 }
 
-function Init-Local-Script-To-EnvPath{
-    $pstage=$env:userprofile+"\Repos\Templates\Script\PowerShell"
-    $plocal=$env:userprofile+"\Scripts"
-    $ts=Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
-    $pdump=$env:userprofile+"\Downloads\Dump\Script\"+$ts
+function Add-Local-Script-To-EnvPath{
+    & Write-Host "Add local script to env::path..."
+    & Add-EnvPath $plocal User
+    & Write-Host "Add local script to env::path...done"
+}
 
+function Re-Create-Local-Script{
+    # Dump
     # Create local script folder if not exist
     New-Item -ItemType Directory -Force -Path $plocal
     # Create dump folder if not exisit
-    New-Item -ItemType Directory -Force -Path $pdump
+    New-Item -ItemType Directory -Force -Path $pdump_local
     # Move current local script to dump
-    & mv $plocal $pdump
+    & mv $plocal $pdump_local
 
     # Create local script folder if not exist
     New-Item -ItemType Directory -Force -Path $plocal
+}
+
+function Sync-Local-Script-From-Template-Repo{
+    & Re-Create-Local-Script
+
     # Copy scripts from pstage(template repo/script folder)
-    echo $pstage
-    cp $pstage/* $plocal
+    & cp $pstage/* $plocal
 
     # Add local script to env::path
     & Add-Local-Script-To-EnvPath
@@ -104,14 +117,30 @@ function Init-Local-Script-To-EnvPath{
     # Close and restart
     & powershell
 
-    & Write-Host "Init local script folder...done"
+    & Write-Host "Init/Sync local script folder...done"
 }
 
-function Add-Local-Script-To-EnvPath{
-    & Write-Host "Add local script to env::path..."
-    $plocal=$env:userprofile+"\Scripts"
-    & Add-EnvPath $plocal User
-    & Write-Host "Add local script to env::path...done"
+function Re-Create-Template-Repo{
+    # Dump
+    # Create dump repo folder if not exisit
+    New-Item -ItemType Directory -Force -Path $pdump_repo
+    # Move current repo script to dump repo folder
+    mv $pstage/* $pdump_repo
+
+    # Create local script folder if not exist
+    New-Item -ItemType Directory -Force -Path $pstage
+}
+
+function Sync-Template-Repo-From-Local-Script{
+    & Re-Create-Template-Repo
+
+    # Copy scripts from local to repo
+    & cp $plocal/* $pstage
+
+    # CD to repo
+    & cd $prepo
+
+    & Write-Host "Sync repo script folder...done"
 }
 
 function Run{
@@ -121,17 +150,22 @@ function Run{
         [string] $Choice
     )
     if($Choice -eq '1'){
-        &Init-Local-Script-To-EnvPath
+        & Sync-Local-Script-From-Template-Repo
+    }
+    elseif($Choice -eq '2'){
+        & Sync-Template-Repo-From-Local-Script
+    }
+    elseif($Choice -eq '0'){
+        #& Re-Create-Template-Repo
     }
     else{
         & Write-Host "Choice no-op yet."
     }
 }
 
-
-
 & Write-Host "Select from:"
-& Write-Host "  1. Init local scripts folder."
-& Write-Host "  2. ???"
-& Write-Host "  0. ???"
+& Write-Host "  1. Init/Sync local from template repo."
+& Write-Host "  2. Sync repo from local script folder."
+& Write-Host "  3. ???"
+& Write-Host "  0. Test"
 & Run
