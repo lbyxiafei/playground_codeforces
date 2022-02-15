fa44b5d1465d28eb1467e18e07015f668a52456f
Bonfire <lbyxiafei@gmail.com>
2022-02-15 11:17:17 -0800

    Check point, 2022-02-15T11:17:16.

--- CP/C.cpp
+++ CP/C.cpp
@@ -32,11 +32,35 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
 
-    int T;
-    cin >> T;
-    forn(_, T) {
-        cout << '?';
+    int n, len, m;
+    cin >> n >> len >> m;
+    vector<int> road(n), speed(n);
+    forn(i,n) cin >> road[i];
+    forn(i,n) cin >> speed[i];
+    road.push_back(len);
+    vector<int> arr, sarr(n+1);
+    forn(i,n){
+        arr.push_back(speed[i]*(road[i+1]-road[i]));
+        sarr[i+1]=sarr[i]+arr.back();
     }
+    int maxx=0;
+    vector<vector<int>> dp(n,vector<int>(m+1));
+    for(int r=0; r<n; r++){
+        vector<int> tmp;
+        for(int l=r-1; l>=0 && r-l<=m; l--){
+            int d=0;
+            for(int k=1; k<=r-l; k++){
+                d+=road[l+2-k]-road[l+1-k];
+                int t=sarr[r+1]-sarr[l]-speed[l]*d;
+                dp[r][k]=max(dp[r][k], dp[l][m-k]+t);
+                maxx=max(maxx, dp[r][k]);
+                cout << r << '-' << k << ':' << maxx << endl;
+            }
+        }
+    }
+    cout << sarr[n]-maxx << endl;
+
+
 
     return 0;
 }
